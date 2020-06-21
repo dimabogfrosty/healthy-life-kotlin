4e6289b23167eba2830ff3965bfdbe77ad4f041e
Dzmitry <dimabogfrosty@gmail.com>
2020-06-22 01:10:58 +0300

    update

--- healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/repository/TrainingRepository.kt
+++ healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/repository/TrainingRepository.kt
@@ -1,12 +1,13 @@
 package by.jcoldsun.healthy_life.repository
 
 import by.jcoldsun.healthy_life.entity.Training
+import by.jcoldsun.healthy_life.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface  TrainingRepository : JpaRepository<Training, Long> {
     fun findByName(name: String): Training?
-    fun findTrainingsByUsersId(id: Long): List<Training>
-    fun findTrainingsByAuthor(authorId: Long): List<Training>
+    fun findTrainingsByUsersId(usersId: User): List<Training>
+    fun findTrainingsByAuthorId(authorId: Long): List<Training>
 }

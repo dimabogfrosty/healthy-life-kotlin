b5364b6a4a1cb669007972a3b098ab19dad3334f
Dzmitry_Bahaslouski <dzmitry.bahaslouski@innowise-group.com>
2020-06-22 16:53:39 +0300

    hot fix

--- healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/entity/Record.kt
+++ healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/entity/Record.kt
@@ -8,7 +8,7 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "records")
-@JsonIgnoreProperties(value = ["runningTime", "user"])
+@JsonIgnoreProperties(value = ["runningTime"])
 data class Record(
         @Id
         @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "record_id_seq")

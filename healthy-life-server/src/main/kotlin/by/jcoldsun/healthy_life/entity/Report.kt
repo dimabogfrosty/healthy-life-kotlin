f7b0bc790ac0afbeea666eb55224c8e0e06fec13
Dzmitry_Bahaslouski <dzmitry.bahaslouski@innowise-group.com>
2020-06-23 15:05:22 +0300

    update report

--- healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/entity/Report.kt
+++ healthy-life-server/src/main/kotlin/by/jcoldsun/healthy_life/entity/Report.kt
@@ -2,7 +2,6 @@ package by.jcoldsun.healthy_life.entity
 
 import java.time.LocalDate
 import java.time.LocalTime
-import kotlin.math.roundToLong
 
 open class Report(private val records: List<Record>) {
     fun getAverageTime(): LocalTime = LocalTime.ofNanoOfDay(getTotalTime().toNanoOfDay() / records.size)
@@ -12,28 +11,18 @@ open class Report(private val records: List<Record>) {
 
     fun getMaxTime(): LocalTime = LocalTime.ofNanoOfDay(runningTimeInNanoOfDay().max(Long::compareTo).get())
 
-    fun getAverageDistance() = (((getTotalDistance() / records.size) * 10).roundToLong() / 10).toDouble()
+    fun getAverageDistance() = getTotalDistance() / records.size
 
-    fun getTotalDistance(): Double {
-        val totalDistance = records.stream().map(Record::distance)
-                .reduce { x: Double, y: Double -> x + y }.get()
-        println("-------------------------------------------------------$totalDistance")
-        return ((totalDistance * 100).roundToLong() / 100).toDouble()
-    }
+    fun getTotalDistance() = records.stream().map(Record::distance)
+            .reduce { x: Double, y: Double -> x + y }.get()
 
     fun getMaxDistance() = records.stream().map(Record::distance).max(Double::compareTo).get()
 
-    fun getAverageSpeed(): Double {
-        val averageSpeed = getAverageDistance() / (getAverageTime().toSecondOfDay() / 3600.toDouble())
-        return ((averageSpeed * 100).roundToLong() / 100).toDouble()
-    }
+    fun getAverageSpeed() = getAverageDistance() / (getAverageTime().toSecondOfDay() / 3600.toDouble())
 
-    fun getMaxSpeed(): Double {
-        val maxSpeed = records.stream()
-                .map { record -> record.distance / (record.getRunningTime().toSecondOfDay() / 3600.toDouble()) }
-                .max(Double::compareTo).get()
-        return ((maxSpeed * 100).roundToLong() / 100).toDouble()
-    }
+    fun getMaxSpeed() = records.stream()
+            .map { record -> record.distance / (record.getRunningTime().toSecondOfDay() / 3600.toDouble()) }
+            .max(Double::compareTo).get()
 
     private fun runningTimeInNanoOfDay() = records.stream()
             .map { record -> record.getRunningTime().toNanoOfDay() }
